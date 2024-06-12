94a14b4747b6c93725a3b78ba0a58a1f4974101f
Andy-B-123 <35752341+Andy-B-123@users.noreply.github.com>
2024-06-12 11:21:24 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bioinformatics_script",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "pandas>=2.0.3"
